190c5abadaade845fd27aa80d62373e76546e2be
FariassArthur <fariasarthur@proton.me>
2026-05-25 14:58:27 -0300

    att dotnet-version

--- FIPE/FIPEBLL.cs
+++ FIPE/FIPEBLL.cs
@@ -1,4 +1,4 @@
-﻿using Newtonsoft.Json;
+using Newtonsoft.Json;
 using RestSharp;
 using System;
 using System.Collections.Generic;
@@ -22,63 +22,47 @@ namespace FIPE
 
         public List<MarcaFIPEinfo> FindMarcasFIPE(string tipo)
         {
-            var client = new RestClient(new Uri("http://fipeapi.appspot.com/"));
-
-            var req = new RestRequest("api/1/{tipo}/marcas.json", Method.Get);
-
+            var client = new RestClient("http://fipeapi.appspot.com/");
+            var req = new RestRequest("api/1/{tipo}/marcas.json");
             req.AddParameter("tipo", tipo, ParameterType.UrlSegment);
-
-            var response = client.Execute(req);
+            var response = client.ExecuteAsync(req).GetAwaiter().GetResult();
             var contentResponse = JsonConvert.DeserializeObject<List<MarcaFIPEinfo>>(response.Content);
-
             return contentResponse;
         }
 
         public List<CarroFIPEinfo> FindCarrosFIPE(string tipo, int idMarca)
         {
-            var client = new RestClient(new Uri("http://fipeapi.appspot.com/")); ;
-
-            var req = new RestRequest("api/1/{tipo}/veiculos/{id}.json", Method.Get);
-
+            var client = new RestClient("http://fipeapi.appspot.com/");
+            var req = new RestRequest("api/1/{tipo}/veiculos/{id}.json");
             req.AddParameter("tipo", tipo, ParameterType.UrlSegment);
             req.AddParameter("id", idMarca, ParameterType.UrlSegment);
-
-            var response = client.Execute(req);
+            var response = client.ExecuteAsync(req).GetAwaiter().GetResult();
             var contentResponse = JsonConvert.DeserializeObject<List<CarroFIPEinfo>>(response.Content);
-
             return contentResponse;
         }
 
         public List<CarroAnoFIPEinfo> FindCarrosAnoFIPE(string tipo, int idMarca, long idCarro)
         {
-            var client = new RestClient(new Uri("http://fipeapi.appspot.com/"));
-
-            var req = new RestRequest("api/1/{tipo}/veiculo/{id}/{idC}.json", Method.Get);
-
+            var client = new RestClient("http://fipeapi.appspot.com/");
+            var req = new RestRequest("api/1/{tipo}/veiculo/{id}/{idC}.json");
             req.AddParameter("tipo", tipo, ParameterType.UrlSegment);
             req.AddParameter("id", idMarca, ParameterType.UrlSegment);
             req.AddParameter("idC", idCarro, ParameterType.UrlSegment);
-
-            var response = client.Execute(req);
+            var response = client.ExecuteAsync(req).GetAwaiter().GetResult();
             var contentResponse = JsonConvert.DeserializeObject<List<CarroAnoFIPEinfo>>(response.Content);
-
             return contentResponse;
         }
 
         public ConsultaFIPEinfo FindPrecoFIPE(string tipo, int idMarca, long idCarro, string idAno)
         {
-            var client = new RestClient(new Uri("http://fipeapi.appspot.com/"));
-
-            var req = new RestRequest("api/1/{tipo}/veiculo/{id}/{idC}/{idA}.json", Method.Get);
-
+            var client = new RestClient("http://fipeapi.appspot.com/");
+            var req = new RestRequest("api/1/{tipo}/veiculo/{id}/{idC}/{idA}.json");
             req.AddParameter("tipo", tipo, ParameterType.UrlSegment);
             req.AddParameter("id", idMarca, ParameterType.UrlSegment);
             req.AddParameter("idC", idCarro, ParameterType.UrlSegment);
             req.AddParameter("idA", idAno, ParameterType.UrlSegment);
-
-            var response = client.Execute(req);
+            var response = client.ExecuteAsync(req).GetAwaiter().GetResult();
             var contentResponse = JsonConvert.DeserializeObject<ConsultaFIPEinfo>(response.Content);
-
             return contentResponse;
         }
     }
